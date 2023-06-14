89b1f8933418e639a5d511cd3193e511d532c301
TajMahal1990 <130366681+TajMahal1990@users.noreply.github.com>
2023-06-14 08:01:20 +0800

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,21 @@
 fun main() {
     println("Анализ приложения")
-    var likes = 71 // количество лайков
-    if(likes == 11 || likes == 12 || likes == 13 || likes == 14) {
-        println("Понравилось $likes людям");
-    } else if (likes % 10 == 1) {
-        println("Понравилось $likes человеку")
+    var likes = 50 // количество лайков
+    if (likes > 100) {
+        if (likes == 11 || likes == 12 || likes == 13 || likes == 14) {
+            println("Понравилось $likes людям");
+        } else if (likes % 100 == 1) {
+            println("Понравилось $likes человеку")
+        } else if (likes % 10 == 1) {
+            println("Понравилось $likes людям")
+        }
     } else {
-        println("Понравилось $likes людям")
+        if(likes == 11 || likes == 12 || likes == 13 || likes == 14) {
+            println("Понравилось $likes людям");
+        } else if (likes % 10 == 1) {
+            println("Понравилось $likes человеку")
+        } else {
+            println("Понравилось $likes людям")
+        }
     }
 }
