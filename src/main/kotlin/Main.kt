5e19f3a5706297642e745ef0830846cd07ff2ab6
TajMahal1990 <130366681+TajMahal1990@users.noreply.github.com>
2023-06-17 19:26:16 +0800

    Update Main.kt3

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,21 +1,7 @@
 fun main() {
     println("Анализ приложения")
-    var likes = 50 // количество лайков
-    if (likes > 100) {
-        if (likes == 11 || likes == 12 || likes == 13 || likes == 14) {
-            println("Понравилось $likes людям");
-        } else if (likes % 100 == 1) {
-            println("Понравилось $likes человеку")
-        } else if (likes % 10 == 1) {
-            println("Понравилось $likes людям")
-        }
-    } else {
-        if(likes == 11 || likes == 12 || likes == 13 || likes == 14) {
-            println("Понравилось $likes людям");
-        } else if (likes % 10 == 1) {
-            println("Понравилось $likes человеку")
-        } else {
-            println("Понравилось $likes людям")
-        }
-    }
-}
+    val likes = 1
+    var okonchan = if ((likes % 10 == 1) && (likes % 100 !==11)) {
+         ("человеку")
+    } else ("людям")
+    println("Понравилось $likes $okonchan") // Упрощение программы
